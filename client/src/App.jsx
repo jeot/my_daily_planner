a6d077b00ef21ea8f7b5e5228845123de261323d
Shamim Keshani <sh.keshani@gmail.com>
2019-12-13 00:11:21 +0330

    fetch api test successful

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,28 +13,14 @@ class App extends Component {
     componentDidMount() {
         console.log("calling backend api...");
         // fetch data from backend
-        this.callBackendApi()
-            .then(res => {
-                this.setState({ data: res.express });
-                console.log("fetch ok.");
-            })
-            .catch(err => {
-                console.log(err);
-                console.log("fetch error.");
-            });
+        this.callApi();
     }
 
-    callBackendApi = async () => {
-        console.log("inside api call...");
-        const response = await fetch("/api");
-        console.log("fetched response:", response);
-        const body = response.json();
-        //const body = await response.body;
-        console.log("body:", body);
-        if (response.status !== 200) {
-            throw Error(body.message);
-        }
-        return body;
+    callApi = async () => {
+        fetch("http://localhost:5000/api")
+            .then(res => res.text())
+            .then(res => this.setState({ data: res }))
+            .catch(err => err);
     };
 
     onDayShift(shift) {
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require("express");
+var cors = require("cors");
+
 var app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.get("/", function(req, res) {
     res.send("hello World!");
 });
 
 app.get("/api", function(req, res) {
-    res.send("we will get you the api!");
+    console.log("some request received:", req);
+    res.send("api is working good!");
 });
 
 app.listen(port, function() {
