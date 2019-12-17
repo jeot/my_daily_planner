e973a32c2c2f23c3075b8b1569dac80fd3d42cd4
Shamim Keshani <sh.keshani@gmail.com>
2019-12-17 19:04:32 +0330

    learn how to redirect Programmatic

--- client/src/pages/List.jsx
+++ client/src/pages/List.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 
 class List extends Component {
+	componentDidMount() {
+		this.timerID = setTimeout(() => {
+			console.log("5 second time out");
+			this.props.history.push("/");
+		}, 5000);
+	}
+	componentWillUnmount() {
+		clearTimeout(this.timerID);
+	}
 	render() {
-		return <div>Some lists here...</div>;
+		return (
+			<div>
+				We don't have anything here. Lets return after 5 seconds! :D
+			</div>
+		);
 	}
 }
 
