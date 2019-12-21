e5bc187488810e6e93a1e732cbb31187cedbac19
Shamim Keshani <sh.keshani@gmail.com>
2019-12-21 18:38:44 +0330

    add web.config: solve express/Plesk problem on serving static files

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const cors = require("cors");
-const dotenv = require("dotenv");
+var express = require("express");
+var app = express();
+var path = require("path");
+var cors = require("cors");
+var dotenv = require("dotenv");
 dotenv.config();
-console.log(`Your port is ${process.env.PORT}`);
-const port = process.env.PORT || 3000;
 
 let users = [{ name: "shamim" }];
+let counter = 0;
 
 app.use(cors());
 app.use(express.json());
 app.use(
 	express.static(path.join(__dirname, "client", "build"), { index: false })
 );
-console.log(path.join(__dirname, "client", "build"));
 
 app.get("/", function(req, res) {
 	console.log("sending index.html file...");
 	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.get("/counter", function(req, res) {
+	counter++;
+	res.json({ counter: counter });
 });
 
 app.get("/users", function(req, res) {
@@ -41,6 +44,9 @@ app.get("/api", function(req, res) {
 	//res.send("api is working.");
 });
 
-app.listen(port, function() {
-	console.log("example app listening on port", port);
-});
+/////////////
+var http = require("http");
+var port = process.env.PORT || 3000;
+console.log(`Your port is ${process.env.PORT}`);
+var server = http.createServer(app);
+server.listen(port);
