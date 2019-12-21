2668f97edc40b5e11265364c7f7414d46e82ec8f
Shamim Keshani <sh.keshani@gmail.com>
2019-12-21 13:09:21 +0330

    fix bug on express serving the index.html file
    delete .env file from being on git

--- server.js
+++ server.js
@@ -5,41 +5,42 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 console.log(`Your port is ${process.env.PORT}`);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-let users = [];
+let users = [{ name: "shamim" }];
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(
+	express.static(path.join(__dirname, "client", "build"), { index: false })
+);
 console.log(path.join(__dirname, "client", "build"));
 
 app.get("/", function(req, res) {
-    // this don't work yet!
-    console.log("sending index.html file...");
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	console.log("sending index.html file...");
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.get("/users", function(req, res) {
-    res.json(users);
+	res.json(users);
 });
 
 app.post("/users", function(req, res) {
-    const new_user = { name: req.body.name, password: req.body.password };
-    users.push(new_user);
-    res.status(201).send();
+	const new_user = { name: req.body.name, password: req.body.password };
+	users.push(new_user);
+	res.status(201).send();
 });
 
 app.get("/api", function(req, res) {
-    console.log("api called.");
-    // send a json:
-    res.send({ a: "hello", b: "goodbye" });
-    //res.json({ a: "A", b: "B"});
+	console.log("api called.");
+	// send a json:
+	res.send({ a: "hello", b: "goodbye" });
+	//res.json({ a: "A", b: "B"});
 
-    // or send a text:
-    //res.send("api is working.");
+	// or send a text:
+	//res.send("api is working.");
 });
 
 app.listen(port, function() {
-    console.log("example app listening on port", port);
+	console.log("example app listening on port", port);
 });
