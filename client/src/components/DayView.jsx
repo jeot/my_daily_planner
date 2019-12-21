8b8a7135a773d4a87cd73f8e6e65e55b5407082a
Shamim Keshani <sh.keshani@gmail.com>
2019-12-21 18:37:09 +0330

    prettify!

--- client/src/components/DayView.jsx
+++ client/src/components/DayView.jsx
@@ -1,32 +1,32 @@
 import React, { Component } from "react";
 
 class DayView extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = { date: props.date };
-    // }
+	// constructor(props) {
+	//     super(props);
+	//     this.state = { date: props.date };
+	// }
 
-    render() {
-        return (
-            <div className="card m-2">
-                <div className="card-body">
-                    <h5 className="card-title">
-                        {this.props.date.toDateString()}
-                    </h5>
-                    <h6 className="card-subtitle mb-2 text-muted">
-                        View of the day
-                    </h6>
-                    <p className="card-text">
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
-                    </p>
-                    <a href="google.com" className="card-link">
-                        Card link
-                    </a>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="card m-2">
+				<div className="card-body">
+					<h5 className="card-title">
+						{this.props.date.toDateString()}
+					</h5>
+					<h6 className="card-subtitle mb-2 text-muted">
+						View of the day
+					</h6>
+					<p className="card-text">
+						T! Some quick example text to build on the card title
+						and make up the bulk of the card's content.
+					</p>
+					<a href="google.com" className="card-link">
+						Card link
+					</a>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default DayView;
--- client/src/pages/MyDailyPlannerApp.jsx
+++ client/src/pages/MyDailyPlannerApp.jsx
@@ -5,53 +5,53 @@ import TodayInformation from "../components/TodayInformation";
 
 const API_URL = "/api";
 class MyDailyPlannerApp extends Component {
-    state = { date: new Date() };
-    constructor(props) {
-        super(props);
-        this.onDayShift = this.onDayShift.bind(this);
-    }
+	state = { date: new Date() };
+	constructor(props) {
+		super(props);
+		this.onDayShift = this.onDayShift.bind(this);
+	}
 
-    componentDidMount() {
-        console.log("calling backend api...");
-        // fetch data from backend
-        this.callApi();
-    }
+	componentDidMount() {
+		console.log("calling backend api...");
+		// fetch data from backend
+		this.callApi();
+	}
 
-    callApi = async () => {
-        let resText = "";
-        let response = null;
-        try {
-            response = await fetch(API_URL);
-            console.log("response:", response);
-            resText = await response.text(); // Parse it as text
-            const data = JSON.parse(resText); // Try to parse it as json
-            // Do your JSON handling here
-            console.log("json data:", data);
-        } catch (err) {
-            // This probably means your response is text, do you text handling here
-            console.log("it is a text:", resText);
-        }
-    };
+	callApi = async () => {
+		let resText = "";
+		let response = null;
+		try {
+			response = await fetch(API_URL);
+			console.log("response:", response);
+			resText = await response.text(); // Parse it as text
+			const data = JSON.parse(resText); // Try to parse it as json
+			// Do your JSON handling here
+			console.log("json data:", data);
+		} catch (err) {
+			// This probably means your response is text, do you text handling here
+			console.log("it is a text:", resText);
+		}
+	};
 
-    onDayShift(shift) {
-        let currentDate = this.state.date;
-        if (shift === 0) currentDate = new Date();
-        else currentDate.setDate(currentDate.getDate() + shift);
-        this.setState({
-            date: currentDate
-        });
-        console.log(this.state);
-    }
+	onDayShift(shift) {
+		let currentDate = this.state.date;
+		if (shift === 0) currentDate = new Date();
+		else currentDate.setDate(currentDate.getDate() + shift);
+		this.setState({
+			date: currentDate
+		});
+		console.log(this.state);
+	}
 
-    render() {
-        return (
-            <div className="container">
-                <TodayInformation date={new Date()} />
-                <DayNavigator onDayShift={this.onDayShift} />
-                <DayView date={this.state.date} />
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="container">
+				<TodayInformation date={new Date()} />
+				<DayNavigator onDayShift={this.onDayShift} />
+				<DayView date={this.state.date} />
+			</div>
+		);
+	}
 }
 
 export default MyDailyPlannerApp;
