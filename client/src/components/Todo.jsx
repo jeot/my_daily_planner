3fe5369143ee4a61ebf2e802d7ef14c3e143f93f
Shamim Keshani <sh.keshani@gmail.com>
2019-12-28 23:43:07 +0330

    use tab instead of spaces in prettify

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -4,26 +4,26 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 
 class Todo extends Component {
-    handleChange = event => {
-        console.log("check state:", event.target.checked);
-    };
+	handleChange = event => {
+		console.log("check state:", event.target.checked);
+	};
 
-    render() {
-        return (
-            <FormControlLabel
-                style={{ width: "100%" }}
-                control={
-                    <Checkbox
-                        checked={this.props.todo.isDone}
-                        color="secondary"
-                        onChange={this.handleChange}
-                        value=""
-                    />
-                }
-                label={this.props.todo.title}
-            />
-        );
-    }
+	render() {
+		return (
+			<FormControlLabel
+				style={{ width: "100%" }}
+				control={
+					<Checkbox
+						checked={this.props.todo.isDone}
+						color="secondary"
+						onChange={this.handleChange}
+						value=""
+					/>
+				}
+				label={this.props.todo.title}
+			/>
+		);
+	}
 }
 
 export default Todo;
